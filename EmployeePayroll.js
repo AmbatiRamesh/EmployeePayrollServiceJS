34c6e97d5486184a9495438d088c2bbe3eff0be7
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-09 13:54:51 +0530

    Refactor: Calculate total employee wage

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -4,20 +4,22 @@ const  PART_TIME_HRS= 4;
 const  PART_TIME= 1;
 const  FULL_TIME= 2;
 const  WAGE_PER_HRS= 20;
-let empHrs=0;
-var empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case PART_TIME:
-        console.log("Employee is Part time");
-        empHrs = PART_TIME_HRS;
-        break;
+function getWorkingHrs(empCheck){
+    switch(empCheck){
+        case PART_TIME:
+            console.log("Employee is Part time");
+            return PART_TIME_HRS;
+            break;
         case FULL_TIME:
             console.log("Employee is Full time");
-            empHrs = FULL_TIME_HRS;
+            return FULL_TIME_HRS;
             break;
-            default:
-                console.log("Employee is Absent");
-                empHrs = 0;
-}
-let empWage = empHrs*WAGE_PER_HRS;
-console.log("EmpWage :"+empWage)
+        default:
+            return 0;
+    }
+    }
+    let empHrs=0;
+    let empCheck=Math.floor(Math.random()*10) % 3;
+    empHrs = getWorkingHrs(empCheck);
+    let empWage = empHrs*WAGE_PER_HRS;
+    console.log("EmpWage : "+empWage)
