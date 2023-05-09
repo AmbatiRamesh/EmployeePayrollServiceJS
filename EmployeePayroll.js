203becf5535a502b5ac5eea88e147693e2dc5b7c
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-09 13:46:12 +0530

    Added: Calculate total employee wage

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,7 +1,23 @@
 console.log("----------Welcome to EmployeeWage---------");
-let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == 1)
-    console.log("Employee is Present");
-else
-    console.log("Employee is Absent");
+const  FULL_TIME_HRS= 8;
+const  PART_TIME_HRS= 4;
+const  PART_TIME= 1;
+const  FULL_TIME= 2;
+const  WAGE_PER_HRS= 20;
+let empHrs=0;
+var empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck){
+    case PART_TIME:
+        console.log("Employee is Part time");
+        empHrs = PART_TIME_HRS;
+        break;
+        case FULL_TIME:
+            console.log("Employee is Full time");
+            empHrs = FULL_TIME_HRS;
+            break;
+            default:
+                console.log("Employee is Absent");
+                empHrs = 0;
+}
+let empWage = empHrs*WAGE_PER_HRS;
+console.log("EmpWage :"+empWage)
