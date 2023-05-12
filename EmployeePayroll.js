302a0bb3e1213e01646e3e8880b6e7bea4c53cb6
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-12 14:05:43 +0530

    Added: Condition of employee wage for a month

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -5,6 +5,7 @@ const  PART_TIME= 1;
 const  FULL_TIME= 2;
 const  WAGE_PER_HRS= 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH =160;
     let monthlyWage=0;
     let empWage=0;
     function getWorkingHrs(empCheck){
@@ -23,11 +24,13 @@ const NUM_OF_WORKING_DAYS = 20;
                break;
         }
         }
-        let empHrs=0;
-        for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+        let empHrs=0;let day = 0; totalWorkingHrs =0;
+        while(day<=NUM_OF_WORKING_DAYS && totalWorkingHrs<=MAX_HRS_IN_MONTH ){
+            totalWorkingHrs++;
         let empCheck=Math.floor(Math.random()*10) % 3;
+        day++;
         empHrs += getWorkingHrs(empCheck);
         empWage = empHrs*WAGE_PER_HRS;
         monthlyWage += empWage;
-    console.log("Total hrs : " + empHrs + " EmpWage : "+empWage + " monthlyWage : " + monthlyWage);
+    console.log("Total hrs : " + totalWorkingHrs + " EmpWage : "+empWage + " monthlyWage : " + monthlyWage);
     }
