327c17f69b11723e205066702cdff88e9ac1af5f
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-16 20:55:58 +0530

    Added: Add Employeepayroll details

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,31 +1,44 @@
 class EmployeePayrollData{
-    //constructor
-    constructor(id, name, salary){
+     //constructor
+     constructor(id, name, salary, gender, startDate){
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
     }
+
     //Getters and setters
-    getId(){ 
-        return this.id; 
-    }
+    getId(){ return this.id; }
     setId(id){
         this.id = id;
     }
-    getName(){ 
-        return this.name; 
-    }
+
+    getName(){ return this.name; }
     setName(name){
         this.name = name;
     }
-    getSalary(){ 
-        return this.salary; 
-    }
+
+    getSalary(){ return this.salary; }
     setSalary(salary){
         this.salary = salary;
     }
+
+    getGender(){ return this.gender; }
+    setGender(gender){
+        this.gender = gender;
+    }
+
+    getStartDate(){ return this.startDate; }
+    setStartDate(startDate){
+        this.startDate = startDate;
+    }
+
     toString(){
-        return "Id = " + this.id + "\nName = " + this.name + "\nSalary = " + this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + ",\tName = " + this.name + ",\tSalary = " + this.salary +
+         ",\tGender = " + this.gender + ",\tStart Date = " + employeeDate;
     }
 }
 //calling the constructor
@@ -35,3 +48,6 @@ employeePayrollData.id = 2;
 employeePayrollData.name = "Chandu";
 employeePayrollData.salary = 68000;
 console.log(employeePayrollData.toString());
+
+let newemployeePayrollData = new EmployeePayrollData(3, "Vinay", 48000, "M", new Date());
+console.log(newemployeePayrollData.toString());
